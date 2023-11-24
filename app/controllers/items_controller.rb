16ecc52917ae6d2aa29c56f2b5ca173476000a24
Kogayo <starry.sky.of.hibarigasaki@gmail.com>
2023-11-24 17:27:09 +0900

    出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
 def index
-  
+  @items = Item.all
 end
 
 def new
+  @item = Item.new
 end
 
 def create
+  @item = Item.new(item_params) 
+
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
 end
+
+
 
 end
 
@@ -18,4 +29,8 @@ def move_to_index
   unless user_signed_in?
     redirect_to action: :index
   end
+end
+
+def item_params
+  params.require(:item).permit(:image, :product_name, :product_description, :category_id, :condition_id, :prefecture_id, :shipping_day_id, :shipping_cost_id, :price).merge(user_id: current_user.id)
 end
