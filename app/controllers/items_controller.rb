a43064c061da5b78d14d9a3863f079f9b4163ca0
Kogayo <starry.sky.of.hibarigasaki@gmail.com>
2023-11-28 19:14:43 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,42 +2,55 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
 
-def index
-  @items = Item.includes(:user).order('created_at DESC')
-end
+  def index
+    @items = Item.includes(:user).order('created_at DESC')
+  end
 
-def new
-  @item = Item.new
-end
+  def new
+    @item = Item.new
+  end
 
-def create
-  @item = Item.new(item_params) 
+  def create
+    @item = Item.new(item_params)
 
-  if @item.valid?
-    @item.save 
-    redirect_to root_path
-  else
-    render :new
+    if @item.valid?
+      @item.save 
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless @item.user == current_user
+      redirect_to root_path
+    end
+  end
+
+  def update 
+    @item = Item.find(params[:id])
+    
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:image, :product_name, :product_description, :category_id, :condition_id, :prefecture_id, :shipping_day_id, :shipping_cost_id, :price).merge(user_id: current_user.id)
   end
 end
-
-def show
-  @item = Item.find(params[:id])
-end
-
-end
-
-private
-
-def move_to_index
-  unless user_signed_in?
-    redirect_to action: :index
-  end
-end
-
-def item_params
-  params.require(:item).permit(:image, :product_name, :product_description, :category_id, :condition_id, :prefecture_id, :shipping_day_id, :shipping_cost_id, :price).merge(user_id: current_user.id)
-end
-
-
-
