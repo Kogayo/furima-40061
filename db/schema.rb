515cbe5ce9bf9d7a9a87c6182f5f789d02b75a45
Kogayo <starry.sky.of.hibarigasaki@gmail.com>
2023-12-04 15:35:57 +0900

    修正6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_30_054824) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_063453) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,9 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_054824) do
     t.datetime "updated_at", null: false
     t.bigint "order_id", null: false
     t.index ["order_id"], name: "index_addresses_on_order_id"
-  end
-
-  create_table "articles", charset: "utf8mb4", force: :cascade do |t|
   end
 
   create_table "conditions", charset: "utf8mb4", force: :cascade do |t|
