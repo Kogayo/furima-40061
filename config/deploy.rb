d6e0076a250944266828da2d0c2a411075d28998
Kogayo <starry.sky.of.hibarigasaki@gmail.com>
2023-12-07 10:38:31 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.18.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima40061'
+set :application, 'furima-40061'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Kogayo/furima-40061.git'
